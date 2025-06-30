0827cfa725e1414f8abc8dc039687091bc50c942
altsem <alltidsemester@pm.me>
2025-07-01 00:00:39 +0200

    rewrite the layout functions, text-wrap / flow layout support

--- src/lib.rs
+++ src/lib.rs
@@ -56,38 +56,36 @@ use arrayvec::ArrayVec;
 use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
-enum Axis {
+enum Orientaiton {
     #[default]
-    X = 0,
-    Y = 1,
+    Horizontal,
+    Vertical,
 }
 
 #[derive(Default, Clone, Copy, PartialEq, Eq, Hash)]
 struct Vec2<T>(T, T);
 
+impl<T> Vec2<T> {
+    fn flip(self) -> Self {
+        Self(self.1, self.0)
+    }
+}
+
+impl<T> From<[T; 2]> for Vec2<T> {
+    fn from([x, y]: [T; 2]) -> Self {
+        Self(x, y)
+    }
+}
+
+impl<T> From<Vec2<T>> for [T; 2] {
+    fn from(val: Vec2<T>) -> Self {
+        [val.0, val.1]
+    }
+}
+
 impl<T: std::fmt::Debug> std::fmt::Debug for Vec2<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Vec2({:?}, {:?})", self.0, self.1)
-    }
-}
-
-impl<T> std::ops::Index<Axis> for Vec2<T> {
-    type Output = T;
-
-    fn index(&self, index: Axis) -> &Self::Output {
-        match index {
-            Axis::X => &self.0,
-            Axis::Y => &self.1,
-        }
-    }
-}
-
-impl<T> std::ops::IndexMut<Axis> for Vec2<T> {
-    fn index_mut(&mut self, index: Axis) -> &mut Self::Output {
-        match index {
-            Axis::X => &mut self.0,
-            Axis::Y => &mut self.1,
-        }
     }
 }
 
@@ -100,8 +98,7 @@ pub struct Layout<'a, T> {
 #[derive(Debug)]
 pub struct LayoutMeta<T> {
     data: Option<T>,
-    orientation: Axis,
-    sizing: Vec2<Sizing>,
+    orientation: Orientaiton,
     gap: usize,
     computed_position: Vec2<usize>,
     computed_size: Vec2<usize>,
@@ -112,8 +109,7 @@ impl<T> Default for LayoutMeta<T> {
     fn default() -> Self {
         Self {
             data: None,
-            orientation: Axis::X,
-            sizing: Vec2::default(),
+            orientation: Orientaiton::Horizontal,
             gap: 0,
             computed_position: Vec2::default(),
             computed_size: Vec2::default(),
@@ -132,14 +128,7 @@ impl<T> LayoutMeta<T> {
 
     pub fn vertical(self) -> Self {
         Self {
-            orientation: Axis::Y,
-            ..self
-        }
-    }
-
-    pub fn sizing(self, x_sizing: Sizing, y_sizing: Sizing) -> Self {
-        Self {
-            sizing: Vec2(x_sizing, y_sizing),
+            orientation: Orientaiton::Vertical,
             ..self
         }
     }
@@ -156,19 +145,55 @@ pub enum Sizing {
     Fixed(usize),
 }
 
-pub struct LayoutIterator<'a, T, const STACK_SIZE: usize> {
-    stack_buffer: ArrayVec<&'a Layout<'a, T>, STACK_SIZE>,
+/// An iterator that traverses trees of [Layout]
+/// It keeps track of its current path in the tree
+/// with a buffer of size `MAX_DEPTH`. This value needs
+/// to be at least the depth of the tree.
+pub struct LayoutIterator<'a, T, const MAX_DEPTH: usize> {
+    root: Option<&'a Layout<'a, T>>,
+    path: ArrayVec<(&'a Layout<'a, T>, usize), MAX_DEPTH>,
 }
 
-impl<'a, T, const STACK: usize> Iterator for LayoutIterator<'a, T, STACK> {
-    type Item = &'a Layout<'a, T>;
+impl<'a, T, const MAX_DEPTH: usize> Iterator for LayoutIterator<'a, T, MAX_DEPTH> {
+    type Item = ([usize; 2], Option<&'a T>);
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.stack_buffer.pop().inspect(|layout| {
-            for child in layout.children.iter() {
-                self.stack_buffer.try_push(child).expect("Layout::iter");
+        if let Some(root) = self.root.take() {
+            if !root.children.is_empty() {
+                self.path.push((root, 0));
             }
-        })
+
+            if root.meta.out_of_bounds {
+                return None;
+            }
+
+            return Some(([0, 0], root.meta.data.as_ref()));
+        }
+
+        let (parent, parent_index) = self.path.last_mut()?;
+        let Some(node) = parent.children.get(*parent_index) else {
+            self.path.pop();
+            return self.next();
+        };
+
+        if node.meta.out_of_bounds {
+            return None;
+        }
+
+        *parent_index += 1;
+
+        if !node.children.is_empty() {
+            self.path.push((node, 0));
+        }
+
+        let position = self
+            .path
+            .iter()
+            .map(|(p, _)| p.meta.computed_position)
+            .chain([node.meta.computed_position])
+            .fold(Vec2(0, 0), |acc, pos| Vec2(acc.0 + pos.0, acc.1 + pos.1));
+
+        Some(([position.0, position.1], node.meta.data.as_ref()))
     }
 }
 
@@ -187,9 +212,11 @@ impl<'a, T: AsRef<str>> Layout<'a, T> {
         let len = text.as_ref().graphemes(true).count();
 
         Self {
-            meta: LayoutMeta::default()
-                .data(text)
-                .sizing(Sizing::Fixed(len), Sizing::Fixed(1)),
+            meta: LayoutMeta {
+                data: Some(text),
+                computed_size: Vec2(len, 1),
+                ..Default::default()
+            },
             children: &mut [],
         }
     }
@@ -198,8 +225,64 @@ impl<'a, T: AsRef<str>> Layout<'a, T> {
     ///
     /// This function recursively updates the inner state of the [Layout].
     pub fn compute(&mut self, bounds: [usize; 2]) {
-        self.compute_axis(Axis::X, bounds[0]);
-        self.compute_axis(Axis::Y, bounds[1]);
+        self.meta.computed_position = Vec2(0, 0);
+
+        let flip_if_vertical = |vec: Vec2<usize>| match self.meta.orientation {
+            Orientaiton::Horizontal => vec,
+            Orientaiton::Vertical => vec.flip(),
+        };
+
+        if self.children.is_empty() {
+            // No children, this node should have a computed_size already set
+            return;
+        }
+
+        let [mut x, mut y] = [0, 0];
+        let Vec2(width, height) = flip_if_vertical(Vec2::from(bounds));
+        let mut tallest_within_line = 0;
+        let mut max_width = 0;
+
+        for child in self.children.iter_mut() {
+            child.compute(flip_if_vertical(Vec2::from([width - x, height - y])).into());
+
+            if flip_if_vertical(child.meta.computed_size).0 > width {
+                // Give up, it won't ever fit
+                child.meta.out_of_bounds = true;
+                return;
+            }
+
+            let gap = if x > 0 { self.meta.gap } else { 0 };
+
+            if x + gap + flip_if_vertical(child.meta.computed_size).0 > width {
+                // Skip to the next line
+
+                if y + flip_if_vertical(child.meta.computed_size).1 >= height {
+                    // Out of lines, won't fit
+                    child.meta.out_of_bounds = true;
+                    return;
+                }
+
+                child.meta.computed_position = flip_if_vertical(Vec2(0, y + tallest_within_line));
+
+                let consumed_width = flip_if_vertical(child.meta.computed_size).0;
+                x = consumed_width;
+                y += flip_if_vertical(child.meta.computed_size).1;
+                tallest_within_line = flip_if_vertical(child.meta.computed_size).1;
+                max_width = max_width.max(x);
+
+                continue;
+            }
+
+            child.meta.computed_position = flip_if_vertical(Vec2(x + gap, y));
+
+            let consumed_width = gap + flip_if_vertical(child.meta.computed_size).0;
+            x += consumed_width;
+            tallest_within_line =
+                tallest_within_line.max(flip_if_vertical(child.meta.computed_size).1);
+            max_width = max_width.max(x);
+        }
+
+        self.meta.computed_size = flip_if_vertical(Vec2(max_width, y + tallest_within_line))
     }
 
     /// Returns an iterator over all layout elements and their computed positions.
@@ -208,73 +291,12 @@ impl<'a, T: AsRef<str>> Layout<'a, T> {
     /// traversing the [Layout] tree in depth-first order.
     ///
     /// Be sure to call [Layout::compute()] before iterating.
-    ///
-    /// # Stack size
-    /// `STACK` - Specifies the size of the stack-allocated stack buffer.
-    pub fn iter<const STACK_SIZE: usize>(&'a self) -> impl Iterator<Item = ([usize; 2], &'a T)> {
-        let mut stack = ArrayVec::new();
-        stack.push(self);
-
-        LayoutIterator::<T, STACK_SIZE> {
-            stack_buffer: stack,
+    pub fn iter<const MAX_DEPTH: usize>(&'a self) -> impl Iterator<Item = ([usize; 2], &'a T)> {
+        LayoutIterator::<'a, T, MAX_DEPTH> {
+            root: Some(self),
+            path: ArrayVec::new(),
         }
-        .filter_map(|layout| {
-            if layout.meta.out_of_bounds {
-                return None;
-            }
-
-            layout.meta.data.as_ref().map(|data| {
-                (
-                    [
-                        layout.meta.computed_position.0,
-                        layout.meta.computed_position.1,
-                    ],
-                    data,
-                )
-            })
-        })
-    }
-
-    fn compute_axis(&mut self, axis: Axis, max_size: usize) -> usize {
-        let computed_size = match self.meta.sizing[axis] {
-            Sizing::Auto => self.compute_children(axis, max_size),
-            Sizing::Fixed(fixed_size) => {
-                if fixed_size > max_size {
-                    self.meta.out_of_bounds = true;
-                }
-                let size = fixed_size.min(max_size);
-                self.compute_children(axis, size);
-                size
-            }
-        };
-
-        self.meta.computed_size[axis] = computed_size;
-        computed_size
-    }
-
-    fn compute_children(&mut self, axis: Axis, max_size: usize) -> usize {
-        if self.meta.orientation == axis {
-            self.children.iter_mut().fold(0, |local_position, child| {
-                let local_position = if local_position > 0 {
-                    local_position + self.meta.gap
-                } else {
-                    local_position
-                };
-
-                let position = (self.meta.computed_position[axis] + local_position).min(max_size);
-                child.meta.computed_position[axis] = position;
-                local_position + child.compute_axis(axis, max_size - position)
-            })
-        } else {
-            self.children
-                .iter_mut()
-                .map(|child| {
-                    child.meta.computed_position[axis] = self.meta.computed_position[axis];
-                    child.compute_axis(axis, max_size)
-                })
-                .max()
-                .unwrap_or(0)
-        }
+        .filter_map(|(pos, data)| data.map(|data| (pos, data)))
     }
 }
 
@@ -371,22 +393,40 @@ mod tests {
     }
 
     #[test]
+    fn overflow_to_next_row() {
+        let mut layout = Layout {
+            meta: LayoutMeta::default(),
+            children: &mut [Layout::text("1"), Layout::text("2")],
+        };
+
+        layout.compute([1, 2]);
+        insta::assert_snapshot!(layout.iter::<8>().collect::<TestBuffer>());
+    }
+
+    #[test]
+    fn overflow_to_next_column() {
+        let mut layout = Layout {
+            meta: LayoutMeta::default().vertical(),
+            children: &mut [Layout::text("1"), Layout::text("2")],
+        };
+
+        layout.compute([2, 1]);
+        insta::assert_snapshot!(layout.iter::<8>().collect::<TestBuffer>());
+    }
+
+    #[test]
     fn docs() {
         let mut layout = Layout {
             meta: LayoutMeta::default().vertical().gap(1),
             children: &mut [
                 Layout::text("Layzer - A Lightweight Terminal Layout Library"),
                 Layout {
-                    meta: LayoutMeta::default().vertical(),
-                    children: &mut [
-                        Layout::text("Layzer is a zero-heap-allocation layout library for"),
-                        Layout::text("building nested layouts with discrete positioning"),
-                        Layout::text("and sizing."),
-                        Layout::text("It's designed for terminal UIs, text-based interfaces,"),
-                        Layout::text("and any scenario where you need to compute positions and"),
-                        Layout::text("sizes of rectangular elements."),
-                    ],
-                },
+                        meta: LayoutMeta::default(),
+                        children: &mut "Layzer is a zero-heap-allocation layout library for building nested layouts with discrete positioning, and sizing. It's designed for terminal UIs, text-based interfaces, and any scenario where you need to compute positions and sizes of rectangular elements."
+                            .split_word_bounds()
+                            .map(Layout::text)
+                            .collect::<Vec<_>>(),
+                    },
                 Layout {
                     meta: LayoutMeta::default().gap(2),
                     children: &mut [
@@ -445,7 +485,7 @@ mod tests {
             ],
         };
 
-        layout.compute([100, 100]);
+        layout.compute([80, 60]);
         insta::assert_snapshot!(layout.iter::<16>().collect::<TestBuffer>());
     }
 }
